bfdd791a1cfbed54e025e7a2de0a0c20b6fdcf3f
seiyokentstyle-source <seiyokentstyle-source@users.noreply.github.com>
2026-09-11 08:05:42 +0000

    deploy: ed2d09633674fa22cd401ef530c87155b1c71704

---
+++ _next/static/x_XBl_1jJckOOYSzrKuKB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Flive-data\u002F[hall]\u002F[id]","\u002Fmachines\u002F[id]","\u002Fmachines\u002F[id]\u002F[hall]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
